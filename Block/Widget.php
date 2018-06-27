474d5116feeef6fc82ce2c30bf5edcc74b3dbf0b
Martijn Cuijten <martijn@vrolijkonline.nl>
2018-06-27 15:16:55 +0200

    Added sidebar widget

---
+++ Block/Widget.php
@@ -0,0 +1,34 @@
+<?php
+namespace Vrolijkonline\Riviyo\Block;
+use \Vrolijkonline\Riviyo\Helper\Data;
+ 
+class Widget extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Vrolijkonline\Riviyo\Helper\Data $helper,
+        array $data = []
+    ) {
+ 
+        $this->_helper = $helper;
+ 
+        parent::__construct(
+            $context,
+            $data
+        );
+    }
+
+    public function getRiviyoData()
+    {
+    	if (!$this->_helper->isEnabled()) return false;
+    	if (!$this->_helper->getRiviyoConfig('richsnippet_enable')) return false;
+    	return [
+    		"magento_data" => [
+    			"website_name" => $this->_helper->getRiviyoConfig('richsnippet_webshop_name')
+    		],
+    		"riviyo_data" => $this->_helper->get_data()
+    	];
+        return 'Hello world!';
+    }
+
+}
